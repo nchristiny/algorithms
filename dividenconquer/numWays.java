91555fa6d5c0bbb91f14595c87e5531ea194c1fd
nchristiny <icarus747@gmail.com>
2018-11-01 08:06:47 -0500

    Initial commit

---
+++ dividenconquer/numWays.java
@@ -0,0 +1,10 @@
+/*   Problem 1: Write a divide and conquer (recursive, no loops) algorithm which takes positive integers n and k, as
+well as array D, indexed from 1 to k (in sorted ascending order), and returns the number of ways to make
+change for n cents using (unlimited) coins of denomination.    */
+public class numWays
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
